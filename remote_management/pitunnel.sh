0783e7492f4ea1361b1e2683d44cb66452a4d9fd
minyang <mychen76@gmail.com>
2022-05-29 08:05:53 -0400

    Create pitunnel.sh

---
+++ remote_management/pitunnel.sh
@@ -0,0 +1,10 @@
+
+https://www.pitunnel.com/
+
+For devices running Raspberry Pi OS or Ubuntu Server:
+curl -s https://pitunnel.com/inst/0IBgFkWC6 | sudo python3
+ 
+For devices running Ubuntu Desktop:
+wget -qO- https://pitunnel.com/inst/0IBgFkWC6 | sudo python3
+
+
