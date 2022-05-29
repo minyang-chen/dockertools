4083e665e6415e75b76faa0be7fb0d3dc9d05ed9
minyang <mychen76@gmail.com>
2022-05-29 01:22:55 -0400

    Create install_docker_ub22.sh

---
+++ install_docker_ub22.sh
@@ -0,0 +1,23 @@
+bash -i 
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt-cache policy docker-ce
+
+sudo apt install docker-ce
+
+sudo usermod -aG docker
+
+sudo systemctl status docker
+
+docker info
+
+docker run hello-world
