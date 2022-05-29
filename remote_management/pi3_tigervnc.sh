b624a5ba85b8e47be085c329ded6495f704116ad
minyang <mychen76@gmail.com>
2022-05-29 08:29:50 -0400

    Update pi3_tigervnc.sh

--- remote_management/pi3_tigervnc.sh
+++ remote_management/pi3_tigervnc.sh
@@ -7,3 +7,8 @@ vncserver :1
 #ifconfig
 # conenct
 # 192.168.1.243:5901
+
+#Creating default startup script /home/pi3/.vnc/xstartup
+#Starting applications specified in /home/pi3/.vnc/xstartup
+#Log file is /home/pi3/.vnc/raspberrypi3:1.log
+
