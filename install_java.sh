eb90c9c78e15c34e394e95fe585b0bca974c1d84
minyang <mychen76@gmail.com>
2022-05-29 01:24:53 -0400

    Create install_java.sh

---
+++ install_java.sh
@@ -0,0 +1,11 @@
+ sudo apt install default-jre
+ 
+ java -version 
+  
+ sudo apt install default-jdk
+  
+ ## sudo apt install openjdk-18-jdk
+ 
+ #sudo nano /etc/environment
+ #JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+ 
