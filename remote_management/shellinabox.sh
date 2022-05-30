e4e07c825ce8207ea8ea06223c3896b184186476
minyang <mychen76@gmail.com>
2022-05-29 22:01:34 -0400

    Create shellinabox.sh

---
+++ remote_management/shellinabox.sh
@@ -0,0 +1,11 @@
+  GNU nano 5.4                                                        restart_shellinabox.sh                                                                 
+sudo apt-cache search shellinabox
+sudo apt-get install openssl shellinabox
+
+sudo systemctl restart shellinabox
+
+sudo service shellinabox restart
+
+## sudo vi /etc/default/shellinabox
+## systemctl enable shellinaboxd
+
