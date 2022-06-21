f9c606f1f6d7de18727414a959c666b0a0f3ff4c
minyang <minyang.chen@ea.epson/.com>
2022-06-21 05:13:39 -0400

    zsh

---
+++ shell/zsh_setup.sh
@@ -0,0 +1,13 @@
+
+# install zsh
+sudo apt-get update
+sudo apt-get install zsh -y
+zsh --version
+
+# 1. install for oh-my-zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+
+# 2 set theme
+ZSH_THEME="powerlevel10k/powerlevel10k" in ~/.zshrc
+
